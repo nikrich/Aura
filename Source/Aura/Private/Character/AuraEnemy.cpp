d772c10781531f08de4e0fce2750306768ab8ede
Jannik Richter <jannik811@gmail.com>
2024-02-25 10:37:39 +0200

    Working highlight on mouse hover

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -8,24 +8,30 @@
 AAuraEnemy::AAuraEnemy()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	GetMesh()->CustomDepthStencilValue = 250;
+	Weapon->CustomDepthStencilValue = 250;
 }
 
 void AAuraEnemy::HighlightActor()
 {
+	// Set the custom depth stencil value to 1
+	// This will make the actor render with the custom depth stencil value
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
+
 	bIsHighlighted = true;
 }
 
 void AAuraEnemy::UnhighlightActor()
 {
+	// Set the custom depth stencil value to 0
+	// This will make the actor render normally
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 	bIsHighlighted = false;
 }
 
 void AAuraEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	if (bIsHighlighted)
-	{
-		DrawDebugSphere(GetWorld(), GetActorLocation(), 100.0f, 12, FColor::Red, false,0, 0, 1.0f);
-	}
 }
